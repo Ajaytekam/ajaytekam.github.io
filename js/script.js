4839e4707ff9aa8e3cfdcff5b64d208f12ae22f6
Ajay Kumar <Ajay@gmail.com>
2023-06-12 18:26:50 +0530

    This is first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* ===================== typing animation =========================*/
+/* var typed = new Typed(".typing", {  */
+
+const typed = new Typed('.typing', {
+    strings: ["DevOps", "Automation", "CICD", "Build Pipelines", "Jenkins Pipelines", "Version Control", "Shell Scripting", "Python Automation", "AWS Cloud", "Azure Cloud", "Containers and Microservices", "SDLC Models", "Terraform", "Ansible", "Monitoring","Web Development"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 600,
+    loop: true
+})
